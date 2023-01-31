160f8ad6656e101bf59191d9d1d1033bca3dff65
Anh Nguyet Vu <anngvu@gmail.com>
2023-01-30 22:05:08 -0700

    Revert reticulate

--- install-pkgs.R
+++ install-pkgs.R
@@ -11,7 +11,7 @@ cran <- c(
   "stringr==1.4.0",
   "DT==0.20",
   "jsonlite==1.7.3",
-  "reticulate==1.27",
+  "reticulate==1.23",
   # "shinydashboardPlus==2.0.3",
   "waiter==0.2.5",
   "readr==2.1.1",
@@ -32,7 +32,7 @@ gh <- c(
 # The binary package distributions from R Studio dramatically speed up installation time
 # For Ubuntu 18.04 (Bionic) it's https://packagemanager.rstudio.com/all/__linux__/bionic/latest
 # For Ubuntu 20.04 (Focal)  it's https://packagemanager.rstudio.com/all/__linux__/focal/latest
-options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/bionic/latest", getOption("repos")))
+options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/focal/latest", getOption("repos")))
 
 install.packages("remotes")
 invisible(
