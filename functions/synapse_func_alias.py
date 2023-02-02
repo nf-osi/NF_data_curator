6901200b7c5ada27403d8ab533f3a8c9912f3b39
Anh Nguyet Vu <anngvu@gmail.com>
2023-02-01 18:45:35 -0700

    Remove no longer needed .py to be on par with upstream

--- functions/synapse_func_alias.py
+++
@@ -1,9 +0,0 @@
-import synapseclient
-
-syn = synapseclient.Synapse()
-syn_login = syn.login
-syn_getUserProfile = syn.getUserProfile
-syn_tableQuery = syn.tableQuery
-syn_get = syn.get
-syn_restGET = syn.restGET
-syn_is_certified = syn.is_certified
